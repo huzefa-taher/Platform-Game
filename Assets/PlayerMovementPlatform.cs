5d248072d5dc9df65dd4be26af6d3a2ee42558d9
huzefa <huzefa@umich.edu>
2018-09-28 18:19:41 -0400

    Initial commit

---
+++ Assets/PlayerMovementPlatform.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementPlatform : MonoBehaviour {
+    public float speed;
+    public float jumpPower;
+
+    Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float xVelocity = speed * Input.GetAxis("Horizontal");
+        float yVelocity = rb.velocity.y;
+
+        if(Input.GetAxis("Vertical") > 0 && isGrounded())
+        {
+            yVelocity = jumpPower;
+        }
+        Vector3 Velocity = new Vector3(xVelocity, yVelocity, 0);
+        rb.velocity = Velocity;
+    }
+
+    bool isGrounded()
+    {
+        int layerMask = LayerMask.GetMask("Platform");
+        return Physics.Raycast(transform.position, Vector3.down, 0.6f, layerMask);
+    }
+}
---
+++ Assets/Scripts/EnemyInteraction.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyInteraction : MonoBehaviour {
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Enemy"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
